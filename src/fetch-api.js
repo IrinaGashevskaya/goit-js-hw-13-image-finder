a8bf23b6f9b60aec52a712025e5cee7ac1791af6
IrinaGashevskaya <70062185+IrinaGashevskaya@users.noreply.github.com>
2021-06-05 21:19:23 +0300

    hw13

--- src/fetch-api.js
+++ src/fetch-api.js
@@ -1,23 +1,55 @@
+import template from './templates/photos.hbs';
 const refs = {
-    searchForm: document.querySelector('.js-search-form'),
-    //articlesContainer:document.querySelector('.js-articles-container'),
-    loadMoreBtn: document.querySelector('{data-action="load-more"')
+    input: document.querySelector('#search-form'),
+    gallery: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('[data-action="load-more"]')
     };
-refs.searchForm.addEventListener('submit', onSearch);
+refs.input.addEventListener('input', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
-
+var searchQuery = '';
+var page = 1;
 function onSearch(e) {
-    e.preventDefault();
+    e.preventDefault(); 
+
+ searchQuery = e.currentTarget.elements.query.value;
+
+//const options = {
+    //headers: {
+     
+    //},
+
+//};
+
+const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=21904508-8c5f9bb97b16d01f890abadf2`;
+
+console.log(url);
+fetchPhotos(url);
+
 
 }
-const options = {
-    headers: {
-        Authorization: '21904508-8c5f9bb97b16d01f890abadf2'
-    },
-
-};
-fetch('https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12', options)
-.then(r => r.json())
-.then(console.log);
+function onLoadMore() {
+page+=1;
+  const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=21904508-8c5f9bb97b16d01f890abadf2`;
+fetchPhotos(url);
 
 
+}
+function fetchPhotos(url) {
+  fetch(url)
+  .then(r => r.json())
+  .then(result => {
+    const hits = result.hits;
+    const markup = template(hits);
+    console.log(markup);
+    refs.gallery.innerHTML = markup; 
+    console.log(result)
+    
+    refs.loadMoreBtn.classList.remove('is-hidden');
+    window.scrollTo({
+      top: document.documentElement.offsetHeight,
+      behavior: 'smooth',
+      block: 'end',
+  });
+    
+    });
+}
---
+++ src/index.js
@@ -0,0 +1,1 @@
+import './fetch-api.js';
