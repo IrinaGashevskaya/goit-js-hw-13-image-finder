a5ee2466d938fb109d4f3bc8838beb48aea4fb9f
IrinaGashevskaya <70062185+IrinaGashevskaya@users.noreply.github.com>
2021-06-06 15:06:45 +0300

    Update fetch-api.js

--- src/fetch-api.js
+++ src/fetch-api.js
@@ -1,55 +1,53 @@
-import template from './templates/photos.hbs';
+import template from "./templates/photos.hbs";
 const refs = {
-    input: document.querySelector('#search-form'),
-    gallery: document.querySelector('.gallery'),
-    loadMoreBtn: document.querySelector('[data-action="load-more"]')
-    };
-refs.input.addEventListener('input', onSearch);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
-var searchQuery = '';
+  input: document.querySelector("#search-form"),
+  gallery: document.querySelector(".gallery"),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+};
+refs.input.addEventListener("input", onSearch);
+refs.loadMoreBtn.addEventListener("click", onLoadMore);
+var searchQuery = "";
 var page = 1;
 function onSearch(e) {
-    e.preventDefault(); 
+  e.preventDefault();
 
- searchQuery = e.currentTarget.elements.query.value;
+  searchQuery = e.currentTarget.elements.query.value;
 
-//const options = {
-    //headers: {
-     
-    //},
+  //const options = {
+  //headers: {
 
-//};
+  //},
 
-const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=21904508-8c5f9bb97b16d01f890abadf2`;
+  //};
 
-console.log(url);
-fetchPhotos(url);
+  const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=21904508-8c5f9bb97b16d01f890abadf2`;
 
-
+  console.log(url);
+  fetchPhotos(url);
 }
 function onLoadMore() {
-page+=1;
+  page += 1;
   const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=21904508-8c5f9bb97b16d01f890abadf2`;
-fetchPhotos(url);
-
-
+  fetchPhotos(url);
 }
 function fetchPhotos(url) {
   fetch(url)
-  .then(r => r.json())
-  .then(result => {
-    const hits = result.hits;
-    const markup = template(hits);
-    console.log(markup);
-    refs.gallery.innerHTML = markup; 
-    console.log(result)
-    
-    refs.loadMoreBtn.classList.remove('is-hidden');
-    window.scrollTo({
-      top: document.documentElement.offsetHeight,
-      behavior: 'smooth',
-      block: 'end',
-  });
-    
+    .then((r) => r.json())
+    .then((result) => {
+      const hits = result.hits;
+      const markup = template(hits);
+      console.log(markup);
+      refs.gallery.innerHTML = markup;
+      console.log(result);
+
+      refs.loadMoreBtn.classList.remove("is-hidden");
+      window.scrollTo({
+        top: document.documentElement.offsetHeight,
+        behavior: "smooth",
+        block: "end",
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
 }
